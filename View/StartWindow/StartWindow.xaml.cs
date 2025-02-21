67761e41188081c78bd98d55e3ce8e815ef0aba0
artxmix <artem.artusevskij01@gmail.com>
2025-02-21 12:34:49 +0300

    StartWindow

---
+++ View/StartWindow/StartWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Strunchik.ViewModel.StartWindowViewModel;
+using System.Windows;
+
+namespace Strunchik.View.StartWindow;
+
+public partial class StartWindow : Window
+{
+    public StartWindow()
+    {
+        InitializeComponent();
+
+        this.DataContext = new StartWindowViewModel();
+    }
+
+    private void TitleBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        var viewModel = (StartWindowViewModel)DataContext;
+        viewModel.DragWindowCommand.Execute(this);
+    }
+}
---
+++ ViewModel/StartWindowViewModel/StartWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Strunchik.ViewModel.Commands;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Strunchik.ViewModel.StartWindowViewModel;
+
+public class StartWindowViewModel
+{
+    public ICommand DragWindowCommand { get; set; }
+    public ICommand CloseWindowCommand { get; }
+
+    public StartWindowViewModel()
+    {
+        DragWindowCommand = new RelayCommand(_ => DragWindow(_));
+        CloseWindowCommand = new RelayCommand(_ => CloseWindow(_));
+    }
+
+    private static void CloseWindow(object _)
+    {
+        if (_ is Window window)
+        {
+            window.Close();
+        }
+        else
+        {
+            throw new ArgumentException("Parameter was not a window");
+        }
+    }
+    private static void DragWindow(object _)
+    {
+        if (_ is Window window)
+        {
+            window.DragMove();
+        }
+        else
+        {
+            throw new ArgumentException("Parameter was not a window");
+        }
+    }
+}
