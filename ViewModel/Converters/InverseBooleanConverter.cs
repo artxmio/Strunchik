855a7bbab990b4a2a8cdb18141d4822072ea475b
artxmix <artem.artusevskij01@gmail.com>
2025-02-21 14:39:15 +0300

    save

---
+++ ViewModel/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Strunchik.ViewModel.Converters;
+
+public class InverseBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
