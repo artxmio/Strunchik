64f7c46c2e4acb42682c4a8de557308daa2d8085
artxmix <artem.artusevskij01@gmail.com>
2025-02-24 01:59:08 +0300

    Authorization

--- ViewModel/StartWindowViewModel/StartWindowViewModel.cs
+++ ViewModel/StartWindowViewModel/StartWindowViewModel.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query.Internal;
 using Strunchik.Model.User;
+using Strunchik.View.MainWindow;
 using Strunchik.ViewModel.Commands;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Input;
 
@@ -13,6 +12,10 @@ namespace Strunchik.ViewModel.StartWindowViewModel;
 public class StartWindowViewModel : INotifyPropertyChanged
 {
     private readonly ApplicationContext.ApplicationContext _context;
+    private string _errorRegMessage = "";
+    private string _errorAuthMessage = "";
+    private bool _isEnabledAuthButton = true;
+    private bool _isEnabledRegButton = true;
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -22,18 +25,49 @@ public class StartWindowViewModel : INotifyPropertyChanged
     public ICommand DragWindowCommand { get; }
     public ICommand CloseWindowCommand { get; }
     public ICommand RegistrationCommand { get; }
+    public ICommand AuthorizationCommand { get; }
 
-    private string _errorMessage = "";
-    public string ErrorMessage
+    public string ErrorRegMessage
     {
-        get => _errorMessage;
+        get => _errorRegMessage;
         set
         {
-            if (_errorMessage != value)
+            if (_errorRegMessage != value)
             {
-                _errorMessage = value;
+                _errorRegMessage = value;
                 OnPropertyChanged();
             }
+        }
+    }
+    public string ErrorAuthMessage
+    {
+        get => _errorAuthMessage;
+        set
+        {
+            if (_errorAuthMessage != value)
+            {
+                _errorAuthMessage = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public bool IsEnabledAuthButton
+    {
+        get => _isEnabledAuthButton;
+        set
+        {
+            _isEnabledAuthButton = value;
+            OnPropertyChanged();
+        }
+    }
+    public bool IsEnabledRegButton
+    {
+        get => _isEnabledRegButton;
+        set
+        {
+            _isEnabledRegButton = value;
+            OnPropertyChanged();
         }
     }
 
@@ -51,13 +85,16 @@ public class StartWindowViewModel : INotifyPropertyChanged
         DragWindowCommand = new RelayCommand(_ => DragWindow(_));
         CloseWindowCommand = new RelayCommand(_ => CloseWindow(_));
         RegistrationCommand = new RelayCommand(_ => Registration());
+        AuthorizationCommand = new RelayCommand(_ => Authorization(_));
     }
 
     private void Registration()
     {
+        IsEnabledRegButton = false;
+
         if (NewUser.Email is null || NewUser.Password is null || NewUser.Name is null)
         {
-            ErrorMessage = "Введите все обязательные поля";
+            ErrorRegMessage = "Введите все обязательные поля";
         }
         else
         {
@@ -68,7 +105,7 @@ public class StartWindowViewModel : INotifyPropertyChanged
 
             if (isExist)
             {
-                ErrorMessage = "Пользователь с такой почтой уже существует";
+                ErrorRegMessage = "Пользователь с такой почтой уже существует";
             }
             else
             {
@@ -77,6 +114,37 @@ public class StartWindowViewModel : INotifyPropertyChanged
                 MessageBox.Show("Поздравляю, вы успешно зарегистрировались! Теперь вы можете авторизоваться.", "Успех", MessageBoxButton.OK);
             }
         }
+        IsEnabledRegButton = true;
+    }
+
+    private void Authorization(object _)
+    {
+        IsEnabledAuthButton = false;
+        if (AuthUser.Email is null || AuthUser.Password is null)
+        {
+            ErrorAuthMessage = "Введите все обязательные поля";
+        }
+        else
+        {
+            _context.Users.Load();
+            var users = _context.Users.Local.ToList();
+
+            bool isExist = users.Any(user => user.Email == AuthUser.Email);
+
+            if (!isExist)
+            {
+                ErrorAuthMessage = "Такого пользователя не существуют";
+            }
+            else
+            {
+                var mainWindow = new MainWindow();
+
+                mainWindow.Show();
+
+                CloseWindowCommand.Execute((Window)_);
+            }
+        }
+        IsEnabledAuthButton = true;
     }
 
     private static void CloseWindow(object _)
@@ -90,6 +158,7 @@ public class StartWindowViewModel : INotifyPropertyChanged
             throw new ArgumentException("Parameter was not a window");
         }
     }
+
     private static void DragWindow(object _)
     {
         if (_ is Window window)
